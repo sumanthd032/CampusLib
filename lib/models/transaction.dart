7323eb85f92dbef2495cc98d3df7621e4e1f6ffb
sumanthd032 <sumanthd032@gmail.com>
2025-05-22 00:25:42 +0530

    Ensure fine_paid parsing expects bool in Transaction.fromJson

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -7,6 +7,7 @@ class Transaction {
   final String? returnDate;
   final String status;
   final double fine;
+  final bool finePaid;
 
   Transaction({
     required this.id,
@@ -17,18 +18,20 @@ class Transaction {
     this.returnDate,
     required this.status,
     required this.fine,
+    required this.finePaid,
   });
 
   factory Transaction.fromJson(Map<String, dynamic> json) {
     return Transaction(
-      id: json['id'],
-      userId: json['user_id'],
-      bookId: json['book_id'],
-      borrowDate: json['borrow_date'],
-      dueDate: json['due_date'],
-      returnDate: json['return_date'],
-      status: json['status'],
+      id: json['id'] as int? ?? (throw Exception('Transaction id cannot be null')),
+      userId: json['user_id'] as int? ?? (throw Exception('Transaction user_id cannot be null')),
+      bookId: json['book_id'] as int? ?? (throw Exception('Transaction book_id cannot be null')),
+      borrowDate: json['borrow_date'] as String? ?? (throw Exception('Transaction borrow_date cannot be null')),
+      dueDate: json['due_date'] as String?,
+      returnDate: json['return_date'] as String?,
+      status: json['status'] as String? ?? (throw Exception('Transaction status cannot be null')),
       fine: (json['fine'] as num?)?.toDouble() ?? 0.0,
+      finePaid: json['fine_paid'] as bool? ?? false,  
     );
   }
 }
