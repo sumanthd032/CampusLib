5dcdbf4af3143baf7d3d77f107f2115a5bedd71b
sumanthd032 <sumanthd032@gmail.com>
2025-06-01 22:47:34 +0530

    Delete unwanted files

--- lib/constants/app_config.dart
+++ lib/constants/app_config.dart
@@ -1,5 +1,3 @@
 class AppConfig {
-  // Base URL for API requests
-  // For development, use localhost. For production, replace with your deployed server URL (e.g., https://yourdomain.com/api)
   static const String apiBaseUrl = 'http://localhost:5000/api';
 }
